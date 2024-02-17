cb8c029ac4eec3e89d6586a47abf597cb88fb8e0
kotaooka <115392256+kotaooka@users.noreply.github.com>
2024-02-17 17:57:01 +0900

    Update AssettoClock.py

--- AssettoClock.py
+++ AssettoClock.py
@@ -9,20 +9,22 @@ time_label = 0
 def acMain(ac_version):
     global app_window, time_label
     app_window = ac.newApp("AssettoClock")
-    ac.setSize(app_window, 260, 80)
+    ac.setSize(app_window, 120, 80)
     ac.drawBorder(app_window, 0)
     ac.setBackgroundOpacity(app_window, 0)
-
+    # タイトルバーの上に重ならないように、コンテンツを下にずらす
+    ac.setPosition(app_window, 0, -30)
 
     ac.setBackgroundColor(app_window, 0, 0, 0)
-    ac.drawBackground(app_window, 1)
-    ac.setVisible(app_window, 0)
-
+    ac.drawBackground(app_window, 0)
+    
+    # タイトルバーを非表示にする
+    ac.setIconPosition(app_window, -10000, -10000)  # ウィンドウの位置を画面外に設定    
 
     # 時刻を表示するラベルを作成
     time_label = ac.addLabel(app_window, "")
-    ac.setPosition(time_label, 130, 40)  # 時計の位置をウィジェットの中央に設定
-    ac.setFontSize(time_label, 30)
+    ac.setPosition(time_label, 60, 40)  # 時計の位置をウィジェットの中央に設定
+    ac.setFontSize(time_label, 22)
     ac.setFontAlignment(time_label, "center")
     ac.setFontColor(time_label, 1, 1, 1, 1)  # フォントの色を白に設定
     
