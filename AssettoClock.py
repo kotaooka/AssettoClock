b8cf6fd8afdca67abfbbf26f1c0078a5fcb30709
kotaooka <115392256+kotaooka@users.noreply.github.com>
2024-02-17 17:31:09 +0900

    Update AssettoClock.py

--- AssettoClock.py
+++ AssettoClock.py
@@ -9,14 +9,20 @@ time_label = 0
 def acMain(ac_version):
     global app_window, time_label
     app_window = ac.newApp("AssettoClock")
-    ac.setSize(app_window, 300, 100)
+    ac.setSize(app_window, 260, 80)
     ac.drawBorder(app_window, 0)
     ac.setBackgroundOpacity(app_window, 0)
 
+
+    ac.setBackgroundColor(app_window, 0, 0, 0)
+    ac.drawBackground(app_window, 1)
+    ac.setVisible(app_window, 0)
+
+
     # 時刻を表示するラベルを作成
     time_label = ac.addLabel(app_window, "")
-    ac.setPosition(time_label, 150, 55)  # 時計の位置をウィジェットの中央に設定
-    ac.setFontSize(time_label, 25)
+    ac.setPosition(time_label, 130, 40)  # 時計の位置をウィジェットの中央に設定
+    ac.setFontSize(time_label, 30)
     ac.setFontAlignment(time_label, "center")
     ac.setFontColor(time_label, 1, 1, 1, 1)  # フォントの色を白に設定
     
@@ -31,9 +37,6 @@ def acUpdate(delta_t):
 def acShutdown():
     global app_window
     if app_window != 0:
-        ac.log("Shutting down AssettoClock")
-        ac.removeWidget(app_window)
-        app_window = 0
         ac.log("Shutting down AssettoClock")
         ac.removeWidget(app_window)
         app_window = 0
